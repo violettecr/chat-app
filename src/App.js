c2c85f11da4b2dd79867335faea4d912d552581e
violettecr <violette.devweb@gmail.com>
2019-10-10 15:16:27 +0200

    Quete 3 REACT

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import Contact from './components/Contact.js';
+import ContactList from './components//ContactList/ContactList';
 
 
 
 function App() {
   return(
     <div className="App">
-      <Contact
-        name="Max Tell"
-        online= {true}
-        avatar="https://randomuser.me/api/portraits/men/27.jpg"
-      />
-      <Contact
-        name="Patate Canapé"
-        online={false}
-        avatar="https://randomuser.me/api/portraits/men/65.jpg"
-      />
-      <Contact
-        name="Roger Rogers"
-        online={true}
-        avatar="https://randomuser.me/api/portraits/men/65.jpg"
-      />     
+      <ContactList/>  
     
     </div>
   );
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -10,20 +10,6 @@ const Contact = (props) => {
 			<div className="name">{props.name}</div>
 			<div className="status">
 
-				{props.online ?
-				
-					<div className="toto">
-						<div className="status-online"></div>
-						<div className="status-text">Online</div>
-					</div>
-				
-				: 
-				
-				<div className="toto">
-						<div className="status-offline"></div>
-						<div className="status-text">Offline</div>
-					</div>
-				}
 			</div>
 		</div>
 	</div>
